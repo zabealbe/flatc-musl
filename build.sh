925ec60a38a8701f115c71f0ac59058d9d1614dc
zabealbe <zabeoalberto99@gmail.com>
2020-11-01 17:20:45 +0100

    optimized script for github actions

--- build.sh
+++ build.sh
@@ -3,10 +3,14 @@ OUTPUT_FOLDER="versions"
 git clone https://github.com/google/flatbuffers flatc && cd flatc
 VERSION=$(git describe --abbrev=0 --tags)
 
+FILE=$OUTPUT_FOLDER/flatc_$VERSION
+
+test -f "$FILE"; then
+	echo "Version $VERSION already exists, please remove $FILE if you want to rebuild it!"
+fi
 
 CC="/usr/lib/musl-gcc -static -Os"
 echo "Running CMAKE with CC=$CC"
-
 
 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
 cmake .
@@ -14,8 +18,6 @@ cmake --build .
 
 cd ../
 mkdir $OUTPUT_FOLDER
-
-FILE=$OUTPUT_FOLDER/flatc_$VERSION
 
 mv flatc/flatc $FILE
 echo "Removing build folder.."
